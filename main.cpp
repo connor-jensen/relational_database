25c32f9201c243e731c03485af0208d574ba86d5
Connor Jensen -vm <connor.jensen@live.com>
2018-10-30 21:25:42 -0700

    i don't even know

--- main.cpp
+++ main.cpp
@@ -12,45 +12,76 @@
 
 int main(int argc, char* argv[])
 {
-    // vector<string> fileNames
-    // {
-    //     "1.txt",
-    //     "2.txt",
-    //     "3.txt",
-    //     "4.txt",
-    //     "5.txt",
-    //     "6.txt",
-    //     "7.txt",
-    //     "8.txt",
-    //     "9.txt",
-    //     "10.txt"
-    // };
-    
-    // for (auto i : fileNames) 
-    // {
-    //     cout << "test: " << i << endl;
-    //     Scanner scanner(i);
-    //     vector<Token*> tokens = scanner.lexicalAnalyzer();
-        
-    //     Parser parser = Parser(tokens);
-    //     DatalogProgram* program = parser.datalogParsing();
-        
-    //     //cout << "calling program.toString()" << endl;
-    //     cout << program->toString() << endl;
-        
-    //     delete program;
-    // }
+    vector<string> inputFileNames
+    {
+        "in1.txt",
+        "in2.txt",
+        "in3.txt"
+        // "in4.txt",
+        // "in5.txt",
+        // "in6.txt",
+        // "in7.txt",
+        // "in8.txt",
+        // "in9.txt",
+        // "in10.txt"
+    };
 
-    Scanner scanner(argv[1]);
-    std::vector<shared_ptr<Token>> tokens = scanner.lexicalAnalyzer();
+    vector<string> expectedOutputFileNames
+    {
+        "out1.txt",
+        "out2.txt",
+        "out3.txt"
+        // "out4.txt",
+        // "out1.txt",
+        // "out5.txt",
+        // "out6.txt",
+        // "out7.txt",
+        // "out8.txt",
+        // "out9.txt",
+        // "out10.txt"
+    };
+
+    for(int i = 0; i < inputFileNames.size(); i++) {
+        Scanner scanner("tests/" + inputFileNames[i]);
+        vector<shared_ptr<Token>> tokens = scanner.lexicalAnalyzer();
+
+        Parser parser = Parser(tokens);
+        shared_ptr<DatalogProgram> program = parser.datalogParsing();
+
+        Interpreter interpreter = Interpreter(program);
+        interpreter.createDatabase();
+
+        string output = interpreter.runQueries();
+
+        ifstream expectedOutputFile("expectedOutputs/" + expectedOutputFileNames[i]);
+
+        stringstream ss;
+        ss << expectedOutputFile.rdbuf();
+        string expectedOutput = ss.str();
+
+        cout << "Test " << i << ": ";
+        if (expectedOutput == output) {
+            cout << "PASS" << endl;
+        }
+        else cout << "FAIL" << endl;
+
+        cout << "Expected output:" << endl;
+        cout << expectedOutput << endl << endl;
+
+        cout << "Actual output:" << endl;
+        cout << output;
+    }
+
+    // Scanner scanner(argv[1]);
+    // std::vector<shared_ptr<Token>> tokens = scanner.lexicalAnalyzer();
     
-    Parser parser = Parser(tokens);
-    shared_ptr<DatalogProgram> program = parser.datalogParsing();
+    // Parser parser = Parser(tokens);
+    // shared_ptr<DatalogProgram> program = parser.datalogParsing();
     
-    Interpreter interpreter = Interpreter(program);
-    interpreter.createDatabase();
+    // Interpreter interpreter = Interpreter(program);
+    // interpreter.createDatabase();
     
-    std::cout << interpreter.runQueries();
+    // std::cout << interpreter.runQueries();
     
     //delete program;
     
